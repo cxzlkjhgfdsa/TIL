3411910a2da316997f907246ef0c9ca72916de74
이승헌 <mylifeforzug123@gmail.com>
2022-12-27 08:50:31 +0900

    2022-12-26 file

---
+++ 2022_12/2022_12_26/code/MemberServiceV4Test.java
@@ -0,0 +1,125 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+*  예외 누수 문제 해결
+* SQLException 제거
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV4Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberServiceV4 memberService;
+
+    @TestConfiguration
+    static class TestConfig{
+
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepository memberRepository(){
+//            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
+        }
+
+
+        @Bean
+        MemberServiceV4 memberServiceV4(){
+            return new MemberServiceV4(memberRepository());
+        }
+    }
+
+//    @BeforeEach
+//    void before(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//        memberRepository = new MemberRepositoryV3(dataSource);
+//        memberService = new MemberServiceV3_3(memberRepository);
+//    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    void AopCheck(){
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberRepository class={}", memberRepository.getClass());
+        assertTrue(AopUtils.isAopProxy(memberService));
+        Assertions.assertFalse(AopUtils.isAopProxy(memberRepository));
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransferGood() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertEquals(findMemberA.getMoney(), 8000);
+        assertEquals(findMemberB.getMoney(), 12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransfer() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEX = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEX);
+
+        //when
+        log.info("START TX");
+        assertThrows(IllegalStateException.class, ()-> memberService.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000));
+        log.info("END TX");
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEX.getMemberId());
+        assertEquals(findMemberA.getMoney(), 10000);
+        assertEquals(findMemberB.getMoney(), 10000);
+    }
+
+}
